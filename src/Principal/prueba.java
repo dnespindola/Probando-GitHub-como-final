ad80cce3d0f75b7623ea81da0fb6da17d465d50e
dnespindola <dnespindola@gmail.com>
2014-03-03 21:35:14 -0300

    Solo Se agrego una linea en blanco para probar el mas

--- src/Principal/prueba.java
+++ src/Principal/prueba.java
@@ -10,6 +10,7 @@ public class prueba {
 	public static void main(String[] args) {
 		new Thread (new Splash()).start();
 		//MediadorMenuPrincipal imp=new MediadorMenuPrincipal();
+		
 		// TODO Auto-generated method stub
 
 	}
