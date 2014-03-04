470b3f3fac4bd2eab09d6be36c755c77da96a93c
dnespindola <dnespindola@gmail.com>
2014-03-03 21:40:17 -0300

    Se le sacan las lineas en blanco

--- src/Principal/prueba.java
+++ src/Principal/prueba.java
@@ -10,8 +10,6 @@ public class prueba {
 	public static void main(String[] args) {
 		new Thread (new Splash()).start();
 		//MediadorMenuPrincipal imp=new MediadorMenuPrincipal();
-		
-		
 		// TODO Auto-generated method stub
 
 	}
